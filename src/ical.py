f5ea05ceeb38f46a01bee22ba5f8b1cfd49547d5
Tykok <treportelie12@gmail.com>
2022-10-30 00:20:00 +0200

    Feat - Start to refractor Python files

--- src/ical.py
+++ src/ical.py
@@ -16,6 +16,17 @@ def create_events(calendar: Calendar, date_start: datetime, date_end: datetime, 
   print(calendar.events)
   return calendar
 
+def create_calendar_and_events(datas):
+  calendar = Calendar()
+  for i in datas:
+    description = f"""
+    Intervenant : {i['course'][0]}
+    Code : {i['course_code']}
+    Salle : {i['salle']}
+    """
+    calendar = create_events(calendar, i['day_start'], i['day_end'], i['course'][1], description)
+  return datas
+
 def create_ics(calendar):
   with open('/home/tykok/Projet/IPI-Calendar-Scrap/my.ics', 'w') as f:
     f.writelines(calendar.serialize_iter())
--- src/main.py
+++ src/main.py
@@ -1,20 +1,9 @@
 # argparse for parameters
 from ics import Calendar
 from scrap import main
-from ical import create_events, create_ics
-import os
+from ical import create_events, create_ics, create_calendar_and_events
 
-os.environ['TZ'] = 'Europe/Paris'
 if __name__ == '__main__':
   week = main()
-  calendar = Calendar()
-  for i in week:
-    description = f"""
-    Intervenant : {i['course'][0]}
-    Code : {i['course_code']}
-    Salle : {i['salle']}
-    """
-    # start = f"{i['day']} {i['hour'][0]}:00"
-    # end = f"{i['day']} {i['hour'][1]}:00"
-    calendar = create_events(calendar, i['day_start'], i['day_end'], i['course'][1], description)
+  calendar = create_calendar_and_events(week)
   create_ics(calendar)
