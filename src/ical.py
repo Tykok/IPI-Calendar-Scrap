85987b30ff35dde1dd7c37cef7c933beb7ac6a84
Tykok <treportelie12@gmail.com>
2022-10-30 00:53:06 +0200

    Feat - Start to add Selenium to login automatically

--- src/ical.py
+++ src/ical.py
@@ -13,7 +13,6 @@ def create_events(calendar: Calendar, date_start: datetime, date_end: datetime, 
   e.begin = date_start.astimezone(timezone('Europe/Paris'))
   e.end = date_end.astimezone(timezone('Europe/Paris'))
   calendar.events.add(e)
-  print(calendar.events)
   return calendar
 
 def create_calendar_and_events(datas):
@@ -25,7 +24,7 @@ def create_calendar_and_events(datas):
     Salle : {i['salle']}
     """
     calendar = create_events(calendar, i['day_start'], i['day_end'], i['course'][1], description)
-  return datas
+  return calendar
 
 def create_ics(calendar):
   with open('/home/tykok/Projet/IPI-Calendar-Scrap/my.ics', 'w') as f:
--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 # argparse for parameters
-from ics import Calendar
 from scrap import main
-from ical import create_events, create_ics, create_calendar_and_events
+from ical import create_ics, create_calendar_and_events
 
 if __name__ == '__main__':
   week = main()
